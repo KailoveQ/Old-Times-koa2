b522a63ad8e62b4e008490a334f4e9aeccf37a3b
Kai <1114366961@qq.com>
2021-04-01 18:48:35 +0800

    通过 ctx 实现多个中间件传数据

--- app.js
+++ app.js
@@ -3,23 +3,21 @@ const Koa = require('koa')
 const app = new Koa()
 
 //注册
-// app.use(async (ctx,next)=>{
-//   //ctx 上下文
-//   const a=await next()
-//   console.log(a)
-//   // a.then((res)=>{
-//   //   console.log(res)
-//   // })
-// })
 app.use(async (ctx,next)=>{
-
+  //ctx 上下文
+  console.log(1)
+  await next()
+  const r = ctx.r
+  console.log(r)
+  console.log(2)
+})
+app.use(async (ctx,next)=>{
+  console.log(3)
   const axios =require('axios')
-  const start = Date.now()
   const res =await axios.get('http://bl.talelin.com/v1/classic/latest?appkey=AbhC31IG7ruCDp57')
-  const end= Date.now()
-  console.log(end-start)
-  console.log(res)
-
+  ctx.r = res
+  await next()
+  console.log(4)
   // return 'hello,kai'
 })
 
