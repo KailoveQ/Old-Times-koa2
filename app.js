0b30e29c563c32a45c9fff86bfd51544529dac85
Kai <1114366961@qq.com>
2021-04-02 14:34:40 +0800

    初始化管理器&&process.cwd()

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 const Koa = require('koa')
-const requireDirectory = require('require-directory')
-const Router = require('koa-router')
+
+
+const InitManager =require('./core/init')
 
 const app = new Koa()
 
-requireDirectory(module,'./api',{
-    visit: whenLoadModule
-})
+InitManager.initCore(app)
 
-function whenLoadModule(obj){
-  if (obj instanceof Router){
-    app.use(obj.routes())
-  }
-}
 app.listen(3000)
