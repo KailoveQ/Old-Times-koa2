c3f98695f90447c6f3723002300130c14466042f
Kai <1114366961@qq.com>
2021-04-02 15:42:54 +0800

    使用 api post 发送请求，获取请求的各种参数

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const Koa = require('koa')
-
-
+const parser =require('koa-bodyparser')
 const InitManager =require('./core/init')
 
 const app = new Koa()
 
+app.use(parser())
+process.cwd()
 InitManager.initCore(app)
 
 app.listen(3000)
