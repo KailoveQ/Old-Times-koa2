2325fb7be39448ec5252bf36b5f21a05f21d4e86
Kai <1114366961@qq.com>
2021-04-02 10:31:04 +0800

    多个Router 拆分

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 const Koa = require('koa')
+const book = require('./api/v1/book')
+const classic = require('./api/v1/classic')
 
 const app = new Koa()
 
-//注册
-app.use(async (ctx,next)=>{
-  //ctx 上下文
-  console.log(1)
-  await next()
-  const r = ctx.r
-  console.log(r)
-  console.log(2)
-})
-app.use(async (ctx,next)=>{
-  console.log(3)
-  const axios =require('axios')
-  const res =await axios.get('http://bl.talelin.com/v1/classic/latest?appkey=AbhC31IG7ruCDp57')
-  ctx.r = res
-  await next()
-  console.log(4)
-  // return 'hello,kai'
-})
+app.use(book.routes())
+app.use(classic.routes())
 
 app.listen(3000)
