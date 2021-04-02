dfa32224af434030c2c6413c6639abf18fe42d35
Kai <1114366961@qq.com>
2021-04-02 19:18:56 +0800

    Lin-Validator v1 初次使用七月老师的校验器

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,20 +1,16 @@
 const Router = require('koa-router')
 const router = new Router()
 
+const {PositiveIntegerValidator} = require('../../validators/validator')
+
 router.post('/v1/:id/classic/latest',(ctx,next)=>{
   const path = ctx.params
   const query = ctx.request.query  //问号后面的查询参数
   const headers = ctx.request.header
   const body = ctx.request.body
 
-  if (true){
-    const error = new global.errs.ParameterException()
-    throw error
-  }
-
-  ctx.body = {
-    key:'classic'
-  }
+  const v = new PositiveIntegerValidator().validate(ctx)
+  ctx.body = '成功'
 
 })
 
--- middlewares/exception.js
+++ middlewares/exception.js
@@ -11,6 +11,15 @@ const  catchError = async ( ctx,next)=>{
       }
       ctx.status = error.code
     }
+    else {
+      //这里处理未知异常
+        ctx.body = {
+          msg:'说实话，有点懵，这个错误我不知道咋回事！',
+          error_code: 999,
+          request:`${ctx.method}  ${ctx.path}`
+        }
+        ctx.status = 500 //代表服务器异常
+    }
   }
 }
 
