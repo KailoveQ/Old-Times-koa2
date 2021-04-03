9104000c5eabf5490842ecb08d33511c218eb003
Kai <1114366961@qq.com>
2021-04-03 14:20:01 +0800

    使用mysql 存储数据

---
+++ app/models/user.js
@@ -0,0 +1,22 @@
+const {db} = require('../../core/db')
+
+const {Sequelize,Model} = require('sequelize')
+
+class user extends Model{
+
+}
+
+User.init({
+  id:{
+    type:Sequelize.INTEGER,
+    primaryKey:true,
+    autoIncrement:true
+  },
+  nickname:Sequelize.STRING,
+  email:Sequelize.STRING,
+  password:Sequelize.STRING,
+  openid:{
+    type:Sequelize.STRING(64),
+    unique:true
+  }
+})
--- config/config.js
+++ config/config.js
@@ -1,4 +1,11 @@
 module.exports={
   // prod
-  environment:'dev'
+  environment:'dev',
+  database:{
+    dbName:'7yue',
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'123456',
+  },
 }
