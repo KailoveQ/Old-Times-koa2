df12fc486ec9bb7385837d06117fa2dd2b02288a
Kai <1114366961@qq.com>
2021-04-06 15:28:44 +0800

    模型的set 方法

--- app/api/v1/user.js
+++ app/api/v1/user.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcryptjs')
+
 const Router = require('koa-router')
 const {RegisterValidator} = require('../../validators/validator')
 const {User} = require('../../models/user')
@@ -11,13 +11,11 @@ router.post('/register',async(ctx,next)=>{
   //思维路径
   // 接收参数 校验
   const v = await new RegisterValidator().validate(ctx)
-  const salt = bcrypt.genSaltSync(10)
-  const psw = bcrypt.hashSync(v.get('body.password2'),salt)
   // v.get 获取用户的数据
   const user ={
-    email:v.get('body.email'),
-    password:psw,
-    nickname:v.get('body.nickname')
+    email: v.get('body.email'),
+    password: v.get('body.password2'),
+    nickname: v.get('body.nickname')
   }
   User.create(user)
 })
--- app/models/user.js
+++ app/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const {sequelize} = require('../../core/db')
 
 const {Sequelize,Model} = require('sequelize')
@@ -18,7 +19,16 @@ User.init({
     type:Sequelize.STRING(128),
     unique:true
   },
-  password:Sequelize.STRING,
+  password: {
+    //扩展 设计模式 观察者模式
+    //ES6 Reflect Vue3.0
+    type: Sequelize.STRING,
+    set(val) {
+      const salt = bcrypt.genSaltSync(10)
+      const psw = bcrypt.hashSync(val, salt)
+      this.setDataValue('password', psw)
+    }
+  },
   openid:{
     type:Sequelize.STRING(64),
     unique:true
