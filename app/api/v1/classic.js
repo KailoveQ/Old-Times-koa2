9dead3b3138701969d01ba064b1f062bb449ad37
Kai <1114366961@qq.com>
2021-04-07 13:39:46 +0800

    初步实现点赞和取消点赞

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -6,7 +6,7 @@ const router = new Router({
 
 const {PositiveIntegerValidator} = require('../../validators/validator')
 const {Auth} = require('../../../middlewares/auth')
-
+const {Art} = require('../../models/art')
 router.get('/latest',new Auth(1).m,async (ctx,next)=>{
 
   const flow = await Flow.findOne({
@@ -14,15 +14,15 @@ router.get('/latest',new Auth(1).m,async (ctx,next)=>{
       ['index', 'DESC']
     ]
   })
-  // const art = await Art.getData(flow.art_id, flow.type)
-  // const i = art.get('image')
-  // const t = art.image
-  // const s = art.getDataValue('image')
-  // const likeLatest = await Favor.userLikeIt(
-  //   flow.art_id, flow.type, ctx.auth.uid)
-  // art.setDataValue('index', flow.index)
-  // art.setDataValue('like_status', likeLatest)
-  ctx.body = flow
+  const art = await Art.getData(flow.art_id, flow.type)
+  const i = art.get('image')
+  const t = art.image
+  const s = art.getDataValue('image')
+  const likeLatest = await Favor.userLikeIt(
+    flow.art_id, flow.type, ctx.auth.uid)
+  art.setDataValue('index', flow.index)
+  art.setDataValue('like_status', likeLatest)
+  ctx.body = art
 
 })
 
--- app/validators/validator.js
+++ app/validators/validator.js
@@ -1,29 +1,44 @@
-const {LinValidator ,Rule} = require('../../core/lin-validator-v2')
-const {User} = require('../models/user')
-const {LoginType, ArtType} = require('../lib/enum')
+const {
+  LinValidator,
+  Rule
+} = require('../../core/lin-validator-v2')
+
+const {
+  User
+} = require('../models/user')
+const {
+  LoginType,
+  ArtType
+} = require('../lib/enum')
+
 class PositiveIntegerValidator extends LinValidator {
   constructor() {
     super()
-    this.id =[
-      new Rule('isInt', '需要是正整数', {min: 1}),
+    this.id = [
+      new Rule('isInt', '需要是正整数', {
+        min: 1
+      }),
     ]
   }
 }
-class RegisterValidator extends LinValidator{
+
+class RegisterValidator extends LinValidator {
   constructor() {
-    super();
+    super()
     this.email = [
-      new Rule('isEmail','不符合Email规范')
+      new Rule('isEmail', '不符合Email规范')
     ]
     this.password1 = [
-      new Rule('isLength','密码至少6个字符，最多32个字符',{
-        min:6,
-        max:'32'
+      // 用户指定范围 123456 $^
+      new Rule('isLength', '密码至少6个字符，最多32个字符', {
+        min: 6,
+        max: 32
       }),
       new Rule('matches', '密码不符合规范', '^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]')
+
     ]
-    this.password2= this.password1
-    this.nickname =[
+    this.password2 = this.password1
+    this.nickname = [
       new Rule('isLength', '昵称不符合长度规范', {
         min: 4,
         max: 32
@@ -50,6 +65,7 @@ class RegisterValidator extends LinValidator{
       throw new Error('email已存在')
     }
   }
+
 }
 
 class TokenValidator extends LinValidator {
@@ -96,10 +112,110 @@ class NotEmptyValidator extends LinValidator {
   }
 }
 
+function checkType(vals) {
+  let type = vals.body.type || vals.path.type
+  if (!type) {
+    throw new Error('type是必须参数')
+  }
+  type = parseInt(type)
+
+  if (!LoginType.isThisType(type)) {
+    throw new Error('type参数不合法')
+  }
+}
+
+function checkArtType(vals) {
+  let type = vals.body.type || vals.path.type
+  if (!type) {
+    throw new Error('type是必须参数')
+  }
+  type = parseInt(type)
+
+  if (!ArtType.isThisType(type)) {
+    throw new Error('type参数不合法')
+  }
+}
+
+class Checker {
+  constructor(type) {
+    this.enumType = type
+  }
+
+  check(vals) {
+    let type = vals.body.type || vals.path.type
+    if (!type) {
+      throw new Error('type是必须参数')
+    }
+    type = parseInt(type)
+
+    if (!this.enumType.isThisType(type)) {
+      throw new Error('type参数不合法')
+    }
+
+  }
+}
+
+
+
+class LikeValidator extends PositiveIntegerValidator {
+  constructor() {
+    super()
+    this.validateType = checkArtType
+    // const checker = new Checker(ArtType)
+    // this.validateType = checker.check.bind(checker)
+  }
+}
+
+class ClassicValidator extends LikeValidator {
+
+}
+
+class SearchValidator extends LinValidator {
+  constructor() {
+    super()
+    this.q = [
+      new Rule('isLength', '搜索关键词不能为空', {
+        min: 1,
+        max: 16
+      })
+    ]
+    this.start = [
+      new Rule('isInt', '不符合规范', {
+        min: 0,
+        max: 60000
+      }),
+      new Rule('isOptional', '', 0)
+    ]
+    this.count = [
+      new Rule('isInt', '不符合规范', {
+        min: 1,
+        max: 20
+      }),
+      new Rule('isOptional', '', 20)
+    ]
+
+  }
+}
+
+class AddShortCommentValidator extends PositiveIntegerValidator {
+  constructor() {
+    super()
+    this.content = [
+      new Rule('isLength', '必须在1到12个字符之间', {
+        min: 1,
+        max: 12
+      })
+    ]
+  }
+}
 
 module.exports = {
   PositiveIntegerValidator,
   RegisterValidator,
   TokenValidator,
   NotEmptyValidator,
+  LikeValidator,
+  ClassicValidator,
+  SearchValidator,
+  AddShortCommentValidator
 }
