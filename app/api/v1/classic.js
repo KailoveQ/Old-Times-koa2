f5523066ddb2d00eb6743ebb98f19a7f52a4f3ff
Kai <1114366961@qq.com>
2021-04-07 09:56:59 +0800

    小程序中携带令牌

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,4 +1,5 @@
 const Router = require('koa-router')
+const {Flow} = require('../../models/flow')
 const router = new Router({
   prefix:'/v1/classic'
 })
@@ -7,7 +8,21 @@ const {PositiveIntegerValidator} = require('../../validators/validator')
 const {Auth} = require('../../../middlewares/auth')
 
 router.get('/latest',new Auth(1).m,async (ctx,next)=>{
-  ctx.body = ctx.auth.uid
+
+  const flow = await Flow.findOne({
+    order: [
+      ['index', 'DESC']
+    ]
+  })
+  // const art = await Art.getData(flow.art_id, flow.type)
+  // const i = art.get('image')
+  // const t = art.image
+  // const s = art.getDataValue('image')
+  // const likeLatest = await Favor.userLikeIt(
+  //   flow.art_id, flow.type, ctx.auth.uid)
+  // art.setDataValue('index', flow.index)
+  // art.setDataValue('like_status', likeLatest)
+  ctx.body = flow
 
 })
 
